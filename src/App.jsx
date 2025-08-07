c2c2489afa0c19c91486ba6f4cdbf00c072f67f3
Anishhh-007 <upretianish407@gmail.com>
2025-08-07 18:08:09 +0545

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Home from './pages/Home'
 import KitchenQueue from './pages/KitchenQueue'
 import Settings from './pages/Settings'
 import Analytics from './pages/Analytics'
-import Adminauth from './pages/Adminauth'
+import AdminAuth from './pages/Adminauth'
 import Workerauth from './pages/WorkerLogin'
 import AdminSignup from './pages/AdminSignup'
 import AdminLogin from './pages/AdminLogin'
@@ -29,7 +29,7 @@ const App = () => {
           <Route path='/kitchenqueue' element={<KitchenQueue />} />
           <Route path='/settings' element={<Settings />} />
           <Route path='/analytics' element={<Analytics />} />
-          <Route path='/auth/admin' element = {<Adminauth />} />
+          <Route path='/auth/admin' element = {<AdminAuth />} />
           <Route path='/auth/worker' element = {<Workerauth />} />
           <Route path='/auth/admin/signup' element = {<AdminSignup />} />
           <Route path='/auth/admin/login' element = {<AdminLogin />} />
